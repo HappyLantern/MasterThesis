028b749fb4e6ae9cc9e8c6d84a8b8c28d83fdaa2
Nordengren Olof <olof.nordengren@afconsult.com>
2019-10-04 14:07:27 +0200

    blabla

--- tagger.py
+++ tagger.py
@@ -6,76 +6,59 @@ from shapely import geometry
 from matplotlib import pyplot
 import os
 
+
+tagfile = open("parktags.txt", 'w')
+"""
+with rasterio.open("satellite images/sthlmgreynew.tif") as fullsat: # fördelad på 33469,27868
+    satmask = fullsat.dataset_mask()
+    for geom, val in rasterio.features.shapes(satmask, transform=fullsat.transform):
+        print(geom)
+        full = geometry.shape(geom)
+"""
+
+start_x = 17.959218298
+start_y = 59.362344447
+width = 0.00188751758
+height = -0.00188690578
+x = start_x
+y = start_y
+shapes = []
+with collection("shapefiles etc/sthlmpoly.shp", "r") as input:
+    for point in input:
+        shp = geometry.shape(point['geometry'])
+        shapes.append(shp)
+for i in range(68):
+    x = start_x
+                         # x-led 18.25367104 - 17.959218298 = 0.294452742
+                         # 0.00188751758 per ruta
+    for j in range(156): # y-led 59.362344447 - 59.234034854 = 0.128309593
+                         # 0.00188690578 per ruta
+        coords = [(x,y), (x,y+height), (x+width, y+height), (x+width, y), (x,y)]
+        geom = {'type' : 'Polygon', 'coordinates': [coords]}
+        tile = geometry.shape(geom)
+        for shp in shapes:
+            if not rasterio.coords.disjoint_bounds(tile.bounds, shp.bounds):
+                has_parking_lot = 1
+                print(i*156+j, i, j )
+                break
+            #print(geometry.shape(point['geometry']), point['properties']
+        tagfile.write(i*156+j, has_parking_lot)
+        x += width
+    y += height
+
+"""
 for filename in os.listdir("qgistiles"):
+    has_parking_lot = 0
     if filename.endswith(".tif"):
-        with rasterio.open("qgistiles/"+filename) as tile: #  'w', driver="GTiff"
-            #array = tile.read(1)
+
+        with rasterio.open("qgistiles/"+filename) as tile:
             mask = tile.dataset_mask()
-            #tile.update_tags('coordinates', [[(a,b)]])
-            #print(array.shape)
-            #pyplot.imshow(array,cmap='gray')
-            #pyplot.show()
 
             for geom, val in rasterio.features.shapes(mask, transform=tile.transform):
-                """
-                print(geom)
-                glist = []
-                for coords in geom['coordinates'][0]:
-                    glist.append(coords)
-                    #tile.update_tags('coordinates', [[(a,b)]])
-                geom['coordinates'][0] = glist
-                print(geom)
-                """
-                geom = rasterio.warp.transform_geom("EPSG:4326", "EPSG:32634", geom, precision=9)
+                # geom = rasterio.warp.transform_geom("EPSG:4326", "EPSG:32634", geom, precision=9)
 
                 tileshape = geometry.shape(geom)
-
-            with collection("shapefiles etc/sthlmpoly.shp", "r") as input: # OBS hårdkodat för sthlmgrey
-                for point in input:
-                    """  90, 212 + 14848, 7168
-
-                    334478.176846732, 6580192.332091706 ger  334478-326525 = 7950 i x-led, på 90+14848 = 14938
-                    och 6583620 - 6580192 = 3428 i y-led på 7380, ger 1.87899371, 2.15285881
-
-                    Betyder att givet en koordinat, t ex 335905, 6580682
-                    kan vi subtrahera bort basen (326525.859) till 9380, och multiplicera med 1.879 till 17625 för x
-                    för y, ta också bort basen, till 2938, och multiplicera med 2.153 till 6325.5
-
-                    17625, 6325.5
-
-
-
-                    """
-                    #print(point)
-                    p = rasterio.warp.transform_geom("EPSG:4326", "EPSG:32634", point['geometry'], precision=9)
-
-                    """
-                    enl mätverktyg 14250 till 16770 ger 1.95564912 och 1.995
-
-                    #print(p)
-                    for coords in p['coordinates'][0]: # 326525.8594834,6570069.2548295 är start
-                        coords[0] -= 326800.8594834    # 343906.6801505,6583620.1083060 är slut
-                        coords[1] -= 6583950.1083060   # ger 17380.8207 i x-led, 13550.8535 i y-led fördelat på 33469,27868
-                        coords[0] *= 1.8789937        # dvs multiplicerar vi t ex resten i x-led med 33469/17380.8 = 1.92563
-                        coords[1] *= -2.15285881        # och i y-led blir det 27868/13550.8535 = 2.05655
-                    #print(coords)
-                    """
-                    shp = geometry.shape(p)
-                    if not rasterio.coords.disjoint_bounds(shp.bounds, tileshape.bounds):
-                        #print("\""+tile.name[18:-4] + "\" ", end='')
-                        print(tile.name)
-
-                    #print(geometry.shape(point['geometry']), point['properties']
-
-
-
+                print(geom)
 """
-with rasterio.open("satellite images/sthlmgrey.tif") as tile:
-    mask = tile.dataset_mask()
-    print(tile)
-    for geom, val in rasterio.features.shapes(mask, transform=tile.transform):
-        print(geom)
 
-        full = geometry.shape(geom)
-
-"""
+tagfile.close()
